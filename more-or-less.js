a9ae17a002bf93ead42dd9f07b59aa3204abd288
njaberi <nezarajaberi@gmail.com>
2024-08-29 19:37:45 +0300

    eh give or take

---
+++ more-or-less.js
@@ -0,0 +1,15 @@
+export function more(num) {
+    return num + 1;
+  }
+  
+  export function less(num) {
+    return num - 1;
+  }
+  
+  export function add(a, b) {
+    return a + b;
+  }
+  
+  export function sub(a, b) {
+    return a - b;
+  }
