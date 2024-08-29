0b8e8ff862d4d2d95457ee0fc3df532acab17c18
njaberi <nezarajaberi@gmail.com>
2024-08-29 20:10:41 +0300

    bruh

--- cut-corners.js
+++ cut-corners.js
@@ -3,22 +3,31 @@ export function round(number) {
   }
   
   export function ceil(number) {
-    const intPart = trunc(number);
-    return intPart < number ? intPart + 1 : intPart;
+    const integer = trunc(number);
+    return number > integer ? integer + 1 : integer;
   }
   
   export function floor(number) {
-    const intPart = trunc(number);
-    return intPart > number ? intPart - 1 : intPart;
+    const integer = trunc(number);
+    return number < integer ? integer - 1 : integer;
   }
   
   export function trunc(number) {
     if (number === 0) return 0;
     if (number < 0) return -trunc(-number);
     
-    let result = 0;
-    while (result + 1 <= number) {
-      result += 1;
+    let exp = 0;
+    while (10 ** exp <= number) {
+      exp++;
     }
+    
+    let result = 0;
+    for (let i = exp - 1; i >= 0; i--) {
+      const base = 10 ** i;
+      while (result + base <= number) {
+        result += base;
+      }
+    }
+    
     return result;
   }
