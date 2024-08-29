f94931f5866195d21f8e5ffb4bc5a5690eae6ab7
njaberi <nezarajaberi@gmail.com>
2024-08-29 19:34:54 +0300

    fixed mutability im so dumb gg

--- mutability.js
+++ mutability.js
@@ -1,16 +1,10 @@
-const person = {
-    name: 'John',
-    age: 30,
-    country: 'GB'
-  };
-  
-  const clone1 = { ...person };
-  
-  const clone2 = Object.assign({}, person);
-  
-  const samePerson = person;
-  
-  person.age++;
-  person.country = 'FR';
-  
-  export { person, clone1, clone2, samePerson };
+const clone1 = { ...person };
+
+const clone2 = Object.assign({}, person);
+
+const samePerson = person;
+
+person.age++;
+person.country = 'FR';
+
+export { person, clone1, clone2, samePerson };
