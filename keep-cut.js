7e4e01d95f41734f6a78c55ff901c5b3ecfaeaeb
njaberi <nezarajaberi@gmail.com>
2024-08-29 19:47:06 +0300

    cut

---
+++ keep-cut.js
@@ -0,0 +1,23 @@
+export function cutFirst(str) {
+    return str.slice(2);
+  }
+  
+  export function cutLast(str) {
+    return str.slice(0, -2);
+  }
+  
+  export function cutFirstLast(str) {
+    return str.slice(2, -2);
+  }
+  
+  export function keepFirst(str) {
+    return str.slice(0, 2);
+  }
+  
+  export function keepLast(str) {
+    return str.slice(-2);
+  }
+  
+  export function keepFirstLast(str) {
+    return keepFirst(str) + keepLast(str);
+  }
