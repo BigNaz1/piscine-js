91ba59a11441455ad93bd395c0151808a2bca76c
njaberi <nezarajaberi@gmail.com>
2024-08-30 17:03:27 +0300

    Egypt2

--- pyramid.js
+++ pyramid.js
@@ -3,8 +3,7 @@ export function pyramid(char, height) {
     for (let i = 0; i < height; i++) {
       const spaces = ' '.repeat(height - i - 1);
       const chars = char.repeat(2 * i + 1);
-      result += spaces + chars;
-      if (i < height - 1) result += '\n';
+      result += spaces + chars + '\n';
     }
     return result;
   }
