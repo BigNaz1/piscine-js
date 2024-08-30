b65be2be161044ae9b68348db1cc991a2c24e492
njaberi <nezarajaberi@gmail.com>
2024-08-30 17:42:21 +0300

    greed

---
+++ greedy-url.js
@@ -0,0 +1,14 @@
+export function getURL(dataSet) {
+    const urlRegex = /https?:\/\/\S+/g;
+    return dataSet.match(urlRegex) || [];
+  }
+  
+  export function greedyQuery(dataSet) {
+    const greedyRegex = /https?:\/\/\S+\?\S+(?:&\S+){2,}/g;
+    return dataSet.match(greedyRegex) || [];
+  }
+  
+  export function notSoGreedy(dataSet) {
+    const notSoGreedyRegex = /https?:\/\/\S+\?\S+(?:&\S+){1,2}(?!&)/g;
+    return dataSet.match(notSoGreedyRegex) || [];
+  }
