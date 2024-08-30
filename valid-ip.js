9e357adba93de1fc267a2742a2a87fc5339b2216
njaberi <nezarajaberi@gmail.com>
2024-08-30 18:14:11 +0300

    port

--- valid-ip.js
+++ valid-ip.js
@@ -1,7 +1,19 @@
 export function findIP(str) {
-    const ipRegex = /\b(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(?::(?:[1-9]\d{0,3}|[1-5]\d{4}|6(?:[0-4]\d{3}|5(?:[0-4]\d{2}|5(?:[0-2]\d|3[0-5])))))?(?!\d)\b/g;
+    const ipRegex = /\b(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(?::\d{1,5})?\b/g;
     
     return str.match(ipRegex)
-      .filter(ip => !ip.includes('255.255.255.000') && !ip.match(/^0+\./))
-      .map(ip => ip.replace(/^0+(\d)/, '$1').replace(/\.0+(\d)/g, '.$1'));
+      .filter(ip => {
+        const [address, port] = ip.split(':');
+        const octets = address.split('.');
+        
+        if (octets.some(octet => octet.length > 1 && octet.startsWith('0'))) {
+          return false;
+        }
+        
+        if (port && (parseInt(port) > 65535 || port.startsWith('0'))) {
+          return false;
+        }
+        
+        return true;
+      });
   }
