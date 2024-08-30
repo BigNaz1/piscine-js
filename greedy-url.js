e7868c0df96d4e442432e05ed7b927787ca6a3fd
njaberi <nezarajaberi@gmail.com>
2024-08-30 17:59:01 +0300

    idk

--- greedy-url.js
+++ greedy-url.js
@@ -1,14 +1,25 @@
-function getURL(dataSet) {
-    const urlPattern = /https?:\/\/[^\s]+/g;
-    return dataSet.match(urlPattern) || [];
-}
-
-function greedyQuery(dataSet) {
-    const greedyPattern = /https?:\/\/[^\s\?#]+(\?[^#\s]*&[^#\s]*&[^#\s]*&[^\s#]*)/g;
-    return dataSet.match(greedyPattern) || [];
-}
-
-function notSoGreedy(dataSet) {
-    const notSoGreedyPattern = /https?:\/\/[^\s\?#]+(\?[^#\s]*&[^#\s]*&[^#\s]*)(?![^#\s]*&)/g;
-    return dataSet.match(notSoGreedyPattern) || [];
-}
+function extractURLs(dataSet) {
+    const urlRegex = /https?:\/\/[^\s]+/g;
+    return dataSet.match(urlRegex) || [];
+  }
+  
+  function countQueryParams(url) {
+    const queryString = url.split('?')[1];
+    if (!queryString) return 0;
+    return queryString.split('&').length;
+  }
+  
+  export function getURL(dataSet) {
+    return extractURLs(dataSet);
+  }
+  
+  export function greedyQuery(dataSet) {
+    return extractURLs(dataSet).filter(url => countQueryParams(url) >= 3);
+  }
+  
+  export function notSoGreedy(dataSet) {
+    return extractURLs(dataSet).filter(url => {
+      const paramCount = countQueryParams(url);
+      return paramCount >= 2 && paramCount <= 3;
+    });
+  }
