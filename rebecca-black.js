7ec61d6d8e50e12f62eed1310d1607cab02004d3
Nezar Jaberi <njaberi@noreply.learn.reboot01.com>
2024-09-03 21:33:25 +0000

    black

---
+++ rebecca-black.js
@@ -0,0 +1,13 @@
+function isFriday(date) {
+    return date.getDay() === 5;
+  }
+  function isWeekend(date) {
+    return date.getDay() === 0 || date.getDay() === 6;
+  }
+  function isLeapYear(date) {
+    let year = date.getFullYear();
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+  }
+  function isLastDayOfMonth(date) {
+    return date.getDate() === new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+  }
