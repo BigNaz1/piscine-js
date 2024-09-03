e76deda399d56654162612054ade9d70b0f9e3e9
Nezar Jaberi <njaberi@noreply.learn.reboot01.com>
2024-09-03 21:14:39 +0000

    date is?

---
+++ date-is.js
@@ -0,0 +1,16 @@
+function isValid(date) {
+    
+    return !(isNaN(date) || date == 0);
+  }
+  function isAfter(date, dateToCompare) {
+    return isValid(date) && isValid(dateToCompare) && date > dateToCompare;
+  }
+  function isBefore(date, dateToCompare) {
+    return isValid(date) && isValid(dateToCompare) && date < dateToCompare;
+  }
+  function isFuture(date) {
+    return isValid(date) && date > new Date();
+  }
+  function isPast(date) {
+    return isValid(date) && date < new Date();
+  }
