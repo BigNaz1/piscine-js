b307b9933c411de99db2d2c08708d0581fde8abb
njaberi <nezarajaberi@gmail.com>
2024-09-04 16:59:56 +0300

    def value

--- using-reduce.js
+++ using-reduce.js
@@ -1,6 +1,8 @@
 const adder = (arr, initial = 0) => arr.reduce((sum, num) => sum + num, initial);
 
-const sumOrMul = (arr, initial = 1) => arr.reduce((result, num) => 
-  num % 2 === 0 ? result * num : result + num, initial);
+const sumOrMul = (arr, initial) => arr.reduce((result, num) => 
+  num % 2 === 0 ? result * num : result + num, 
+  initial !== undefined ? initial : arr[0]
+);
 
 const funcExec = (arr, initial) => arr.reduce((result, func) => func(result), initial);
