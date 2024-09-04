16330d9474698b55da929cc41616c210dade28f6
njaberi <nezarajaberi@gmail.com>
2024-09-04 17:04:43 +0300

    ...

--- using-reduce.js
+++ using-reduce.js
@@ -1,8 +1,17 @@
 const adder = (arr, initial = 0) => arr.reduce((sum, num) => sum + num, initial);
 
-const sumOrMul = (arr, initial) => arr.reduce((result, num) => 
-  num % 2 === 0 ? result * num : result + num, 
-  initial !== undefined ? initial : arr[0]
-);
+const sumOrMul = (arr, initial) => {
+  if (initial !== undefined) {
+    return arr.reduce((result, num) => 
+      num % 2 === 0 ? result * num : result + num, 
+      initial
+    );
+  } else {
+    return arr.slice(1).reduce((result, num) => 
+      num % 2 === 0 ? result * num : result + num, 
+      arr[0]
+    );
+  }
+};
 
 const funcExec = (arr, initial) => arr.reduce((result, func) => func(result), initial);
