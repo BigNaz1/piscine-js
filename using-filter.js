4865ef236408cc48b0f8f9fecc7475b50ae4ed48
njaberi <nezarajaberi@gmail.com>
2024-09-04 16:53:29 +0300

    new reading correctly

--- using-filter.js
+++ using-filter.js
@@ -5,8 +5,8 @@ const filterStartVowel = (arr) => arr.filter(str => /^[aeiou]/i.test(str));
 const filter5Vowels = (arr) => arr.filter(str => (str.match(/[aeiou]/gi) || []).length >= 5);
 
 const filter1DistinctVowel = (arr) => arr.filter(str => {
-  const vowels = str.match(/[aeiou]/gi) || [];
-  return new Set(vowels).size === 1;
+  const vowels = str.toLowerCase().match(/[aeiou]/g) || [];
+  return vowels.length > 0 && new Set(vowels).size === 1;
 });
 
 const multiFilter = (arr) => arr.filter(obj => 
