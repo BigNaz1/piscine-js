2bf765e7947a73798782a1f1c82dccd235f6b830
Nezar Jaberi <njaberi@noreply.learn.reboot01.com>
2024-09-05 23:16:08 +0000

    map

---
+++ mapper.js
@@ -0,0 +1,11 @@
+function map(arr, fn) {
+    let result = []
+    for (let i = 0; i < arr.length; i++) {
+      result.push(fn(arr[i], i, arr))
+    }
+    return result
+  }
+  
+  function flatMap(arr, fn) {
+    return arr.reduce((acc, val, i, arr) => acc.concat(fn(val, i, arr)), [])
+  }
