3f47d717efc085b6ccf2f9c90405bacdc2569a52
Nezar Jaberi <njaberi@noreply.learn.reboot01.com>
2024-09-15 03:38:39 +0000

    I wanna sleep

--- interpolation.js
+++ interpolation.js
@@ -2,19 +2,12 @@ function interpolation({ step, start, end, callback, duration }) {
   const stepSize = (end - start) / step;
   const timeStep = duration / step;
 
-  let currentStep = 0;
+  for (let i = 0; i < step; i++) {
+    const distance = start + i * stepSize;
+    const point = i * timeStep;
 
-  function executeStep() {
-    if (currentStep < step) {
-      const distance = start + currentStep * stepSize;
-      const point = currentStep * timeStep;
-
+    setTimeout(() => {
       callback([distance, point]);
-
-      currentStep++;
-      setTimeout(executeStep, timeStep);
-    }
+    }, point);
   }
-
-  executeStep();
 }
