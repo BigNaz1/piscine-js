ce8cf902a9ca3b6c6a98b2307b62bead1a8867e6
Nezar Jaberi <njaberi@noreply.learn.reboot01.com>
2024-09-15 03:43:29 +0000

    Should I just end it all?

---
+++ keep-trying-or-giveup.js
@@ -0,0 +1,32 @@
+function retry(count, callback) {
+  return async (...args) => {
+    let attempts = 0;
+    while (true) {
+      try {
+        return await callback(...args);
+      } catch (error) {
+        attempts++;
+        if (attempts > count) {
+          throw new Error('Max retries reached');
+        }
+      }
+    }
+  };
+}
+
+function timeout(delay, callback) {
+  return async (...args) => {
+    const timeoutPromise = new Promise((_, reject) => {
+      setTimeout(() => reject(new Error('timeout')), delay);
+    });
+
+    try {
+      return await Promise.race([callback(...args), timeoutPromise]);
+    } catch (error) {
+      if (error.message === 'timeout') {
+        return error;
+      }
+      throw error;
+    }
+  };
+}
