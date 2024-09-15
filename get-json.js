19697fc3504a68b36ae54964456fc5f722c57c57
Nezar Jaberi <njaberi@noreply.learn.reboot01.com>
2024-09-15 04:03:38 +0000

    git commit

--- get-json.js
+++ get-json.js
@@ -1,23 +1,25 @@
 async function getJSON(path = '', params = {}) {
-    const queryString = Object.entries(params)
-        .map(([key, value]) => 
-            `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
-        )
-        .join('&');
-    
-    const url = queryString ? `${path}?${queryString}` : path;
-
-    const response = await fetch(url);
-    
-    if (!response.ok) {
-        throw new Error(response.statusText);
-    }
-
-    const res = await response.json();
-
+    const url =
+        path +
+        '?' +
+        Object.keys(params)
+            .map((key) => {
+                return (
+                    key.replace(' ', '+') +
+                    '=' +
+                    params[key].toString().replace(' ', '+')
+                );
+            })
+            .join('&');
+    const res = await fetch(url).then((response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error(response.statusText);
+        }
+    });
     if (res.error) {
         throw new Error(res.error);
     }
-
     return res.data;
 }
