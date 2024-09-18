dd1146c500632d09601a2dfa69431d3905a8ffa2
Nezar Jaberi <njaberi@noreply.learn.reboot01.com>
2024-09-18 06:23:37 +0000

    disco

---
+++ verydisco.mjs
@@ -0,0 +1,17 @@
+const input = process.argv[2];
+
+if (!input) {
+    console.error('Please provide an argument.');
+    process.exit(1);
+}
+
+function makeVeryDisco(word) {
+    const midpoint = Math.ceil(word.length / 2);
+    return word.slice(midpoint) + word.slice(0, midpoint);
+}
+
+const veryDiscoResult = input.split(' ')
+    .map(makeVeryDisco)
+    .join(' ');
+
+console.log(veryDiscoResult);
